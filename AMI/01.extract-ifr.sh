72648c5a2ea92f9e56cc3acb07c89a885c9bc58a
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2026-09-04 01:35:26 +0500

    misc

--- AMI/01.extract-ifr.sh
+++ AMI/01.extract-ifr.sh
@@ -5,10 +5,10 @@ base_dir="$(cd "$(dirname "$0")" && pwd)"
 ifr_dir="$base_dir/boards/imb760/ifr"
 uefi_mod_tools="$base_dir/../SOFTWARE/uefi-mod-tools/uefi-mod-tools"
 
-#exec "$base_dir/../SOFTWARE/ifr-extractor/extract.cs" \
-#  --uefi-dump "$base_dir/boards/imb760/base/IMB760_BIOS.bin" \
-#  --manifest "$base_dir/boards/imb760/ifr/sections.yaml" \
-#  --output-dir "$base_dir/boards/imb760/ifr"
+"$base_dir/../SOFTWARE/ifr-extractor/extract.cs" \
+  --uefi-dump "$base_dir/boards/imb760/base/IMB760_BIOS.bin" \
+  --manifest "$base_dir/boards/imb760/ifr/sections.yaml" \
+  --output-dir "$base_dir/boards/imb760/ifr"
 
 "$uefi_mod_tools" uefi nvar map -i "$ifr_dir/AF516361-BiosDefaults.bin" -o "$ifr_dir/AF516361-BiosDefaults.json"
 
