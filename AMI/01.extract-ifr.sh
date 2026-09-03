551ec73ea3fbd3dd4bad0c10b9ed58d12292c0c6
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2026-09-04 02:54:11 +0500

    add another default values store

--- AMI/01.extract-ifr.sh
+++ AMI/01.extract-ifr.sh
@@ -4,19 +4,27 @@ set -euo pipefail
 base_dir="$(cd "$(dirname "$0")" && pwd)"
 ifr_dir="$base_dir/boards/imb760/ifr"
 uefi_mod_tools="$base_dir/../SOFTWARE/uefi-mod-tools/uefi-mod-tools"
+uefi_extract="$base_dir/../SOFTWARE/UEFITool_NE-cli/uefitool-ne-cli"
+ifr_extractor="$base_dir/../SOFTWARE/IFRExtractor-RS-structured/ifrextractor"
 
-"$base_dir/../SOFTWARE/ifr-extractor/extract.cs" \
-  --uefi-dump "$base_dir/boards/imb760/base/IMB760_BIOS.bin" \
-  --manifest "$base_dir/boards/imb760/ifr/sections.yaml" \
-  --output-dir "$base_dir/boards/imb760/ifr"
-
-"$uefi_mod_tools" uefi nvar map -i "$ifr_dir/AF516361-BiosDefaults.bin" -o "$ifr_dir/AF516361-BiosDefaults.json"
+"$uefi_extract" extract \
+  "$base_dir/boards/imb760/base/IMB760_BIOS.bin" \
+  "$base_dir/boards/imb760/ifr/sections-new.yaml" \
+  "$ifr_dir"
 
 for D in $(find "$ifr_dir" -maxdepth 1 -mindepth 1 -type d -printf '%P\n'); do
-  "$uefi_mod_tools" uefi nvar map-ifr-stores \
-    -i "$ifr_dir/AF516361-BiosDefaults.json" \
-    --ifr "$ifr_dir/${D}/${D}_setup.sct.0.0.uefi.ifr.json" \
-    -o "$ifr_dir/${D}/AF516361-BiosDefaults-nvar-map.json"
+  "$ifr_extractor" "$ifr_dir/${D}/${D}_setup.sct" json
+done
+
+for defaults in AF516361-BiosDefaults 9221315B-BiosDefaults; do
+  "$uefi_mod_tools" uefi nvar map -i "$ifr_dir/${defaults}.bin" -o "$ifr_dir/${defaults}.json"
+
+  for D in $(find "$ifr_dir" -maxdepth 1 -mindepth 1 -type d -printf '%P\n'); do
+    "$uefi_mod_tools" uefi nvar map-ifr-stores \
+      -i "$ifr_dir/${defaults}.json" \
+      --ifr "$ifr_dir/${D}/${D}_setup.sct.0.0.uefi.ifr.json" \
+      -o "$ifr_dir/${D}/${defaults}-nvar-map.json"
+  done
 done
 
 for D in $(find "$ifr_dir" -maxdepth 1 -mindepth 1 -type d -printf '%P\n'); do
